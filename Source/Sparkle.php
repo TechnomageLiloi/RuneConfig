e63c57d8675e1b97762be27969d3ee5030ae25a2
Anton Moskalenko <moskalenko.anton@gmail.com>
2022-12-02 01:08:22 +0200

    [0.0.10] Add sparkle implementation.

--- Source/Sparkle.php
+++ Source/Sparkle.php
@@ -4,5 +4,40 @@ namespace Liloi\Config;
 
 class Sparkle
 {
+    private $f;
 
+    private $key;
+
+    public function __construct(?string $key = null, ?callable $f = null)
+    {
+        if(!is_null($key))
+        {
+            $this->getKey($key);
+        }
+
+        if(!is_null($f))
+        {
+            $this->get($f);
+        }
+    }
+
+    public function getKey(string $key): void
+    {
+        $this->key = $key;
+    }
+
+    public function setKey(): string
+    {
+        return $this->key;
+    }
+
+    public function get(callable $f): void
+    {
+        $this->f = $f;
+    }
+
+    public function set(): callable
+    {
+        return $this->f;
+    }
 }
