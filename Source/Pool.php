b97bb3615df20046ef2bbddab3b023f2864e771a
Anton Moskalenko <moskalenko.anton@gmail.com>
2022-12-05 09:12:04 +0200

    [0.0.14] Add pool set/test tests.

--- Source/Pool.php
+++ Source/Pool.php
@@ -4,5 +4,13 @@ namespace Liloi\Pensieve;
 
 class Pool
 {
+    public static function get(string $key)
+    {
+        throw new \Exception('Not implemented exception.');
+    }
 
+    public static function set(Sparkle $sparkle): void
+    {
+        throw new \Exception('Not implemented exception.');
+    }
 }
